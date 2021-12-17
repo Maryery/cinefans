2a04de8eff38731d71f38c28d02821d7597fa947
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2021-12-17 16:00:04 +0000

    Create swicth for case for service

--- app.js
+++ app.js
@@ -13,3 +13,48 @@ var NewFlix = /** @class */ (function () {
     };
     return NewFlix;
 }());
+var CamaronPrime = /** @class */ (function () {
+    function CamaronPrime() {
+    }
+    CamaronPrime.prototype.Renew = function () {
+        console.log('Renewing CamaronPrime');
+    };
+    CamaronPrime.prototype.Login = function () {
+        console.log('Login to CamaronPrime');
+    };
+    CamaronPrime.prototype.Cancel = function () {
+        console.log('Canceling CamaronPrime');
+    };
+    return CamaronPrime;
+}());
+var OBH = /** @class */ (function () {
+    function OBH() {
+    }
+    OBH.prototype.Renew = function () {
+        console.log('Renewing OBH');
+    };
+    OBH.prototype.Login = function () {
+        console.log('Login to OBH');
+    };
+    OBH.prototype.Cancel = function () {
+        console.log('Canceling OBH');
+    };
+    return OBH;
+}());
+var button = document.getElementById('btn');
+var service = document.getElementById('service');
+var task = document.getElementById('task');
+var selectedService;
+service.addEventListener('change', function () {
+    switch (service.value) {
+        case '1':
+            selectedService = new NewFlix();
+            break;
+        case '2':
+            selectedService = new CamaronPrime();
+            break;
+        case '3':
+            selectedService = new OBH();
+            break;
+    }
+});
--- app.ts
+++ app.ts
@@ -15,3 +15,44 @@ class NewFlix implements Service {
 		console.log('Canceling Newflix');
 	}
 }
+
+class CamaronPrime implements Service {
+	Renew(): void {
+		console.log('Renewing CamaronPrime');
+	}
+	Login(): void {
+		console.log('Login to CamaronPrime');
+	}
+	Cancel(): void {
+		console.log('Canceling CamaronPrime');
+	}
+}
+
+class OBH implements Service {
+	Renew(): void {
+		console.log('Renewing OBH');
+	}
+	Login(): void {
+		console.log('Login to OBH');
+	}
+	Cancel(): void {
+		console.log('Canceling OBH');
+	}
+}
+
+const button = document.getElementById('btn')!;
+const service = <HTMLSelectElement>document.getElementById('service');
+const task = <HTMLSelectElement>document.getElementById('task');
+
+let selectedService: Service;
+
+service.addEventListener('change', () => {
+	switch(service.value){
+		case '1': selectedService = new NewFlix();
+			break;
+		case '2': selectedService = new CamaronPrime();
+			break;
+		case '3': selectedService = new OBH();
+			break;
+	}
+});
